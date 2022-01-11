2c1ce3398b84b7f6917c5ece37a6939b045e1261
rammfall <15bogdan17@gmail.com>
2022-01-12 01:58:17 +0200

    Compelte

---
+++ src/js/example.js
@@ -0,0 +1,43 @@
+const array = [1, 2, 3];
+
+forEach(array, function (item, index, arr) {
+  console.log(item, index);
+})
+// 1 0
+// 2 1
+// 3 2
+
+const mapResult = map(array, function (item, index, array) {
+  return item * 4;
+});
+
+console.log(mapResult) // => [4, 8, 12]
+
+const filterResult = filter(array, (item, index, array) => {
+  return item > 1;
+});
+
+console.log(filterResult); // => [3]
+
+
+const reduceResult = reduce(array, (previous, current, index, array) => {
+  return previous + current;
+}, 0);
+
+console.log(reduceResult); // => 6
+
+
+const someResult = some(array, (item, index, array) => {
+  return item > 2;
+});
+
+console.log(someResult); // => true
+
+
+const everyResult = every(array, (item, index, array) => {
+  return item > 2;
+});
+
+console.log(someResult); // => false
+
+
