be17a90c6ace3dcb0e08a4efd8bee64785a52e34
rammfall <15bogdan17@gmail.com>
2022-01-12 01:13:34 +0200

    Complete tests

--- src/js/__tests__/arrays.spec.js
+++ src/js/__tests__/arrays.spec.js
@@ -1,9 +1,92 @@
-const map = require('../arrays');
+const all = require('../arrays');
 
 describe('Array functions', () => {
+  describe('forEach()', () => {
+    it('should return undefined and was called with right params in callback', () => {
+      const callback = jest.fn();
+      const array = [1, 2, 3];
+
+      expect(all.forEach(array, callback)).toStrictEqual(undefined);
+      expect(callback).toHaveBeenNthCalledWith(1, array[0], 0, array);
+      expect(callback).toHaveBeenNthCalledWith(2, array[1], 1, array);
+      expect(callback).toHaveBeenNthCalledWith(3, array[2], 2, array);
+    })
+  })
+
   describe('map()', () => {
     it('should return new array edited by callback', () => {
-      expect(map([1, 2, 3], (item) => item * 3)).toStrictEqual([3, 6, 9]);
+      const originalArray = [1, 2, 3];
+      const copiedArray = [...originalArray];
+
+      expect(all.map(originalArray, (item) => item * 3)).toStrictEqual([3, 6, 9]);
+      expect(originalArray).toStrictEqual(copiedArray);
+
+      const callback = jest.fn();
+
+      all.map(originalArray, callback);
+      expect(callback).toHaveBeenNthCalledWith(1, originalArray[0], 0, originalArray);
+    })
+  })
+
+  describe('filter()', () => {
+    it('should return filtered new array', () => {
+      const originalArray = [1, 2, 3];
+      const copiedArray = [...originalArray];
+
+      expect(all.filter(originalArray, (item) => item > 2)).toStrictEqual([3]);
+      expect(originalArray).toStrictEqual(copiedArray);
+
+      const callback = jest.fn();
+
+      all.filter(originalArray, callback);
+      expect(callback).toHaveBeenNthCalledWith(1, originalArray[0], 0, originalArray);
+    })
+  })
+
+  describe('reduce()', () => {
+    it('should return filtered new array', () => {
+      const originalArray = [1, 2, 3];
+      const copiedArray = [...originalArray];
+
+      expect(all.reduce(originalArray, (prev, curr) => prev + curr, 0)).toStrictEqual(6);
+      expect(originalArray).toStrictEqual(copiedArray);
+
+      const callback = jest.fn();
+
+      all.map(originalArray, callback);
+      expect(callback).toHaveBeenNthCalledWith(1, originalArray[0], 0, originalArray);
+    })
+  })
+
+  describe('some()', () => {
+    it('should return filtered new array', () => {
+      const originalArray = [1, 2, 3];
+      const copiedArray = [...originalArray];
+
+      expect(all.some(originalArray, (item) => item > 0)).toStrictEqual(true);
+      expect(all.some(originalArray, (item) => item > 3)).toStrictEqual(false);
+      expect(originalArray).toStrictEqual(copiedArray);
+
+      const callback = jest.fn();
+
+      all.map(originalArray, callback);
+      expect(callback).toHaveBeenNthCalledWith(1, originalArray[0], 0, originalArray);
+    })
+  })
+
+  describe('every()', () => {
+    it('should return filtered new array', () => {
+      const originalArray = [1, 2, 3];
+      const copiedArray = [...originalArray];
+
+      expect(all.every(originalArray, (item) => item > 0)).toStrictEqual(true);
+      expect(all.every(originalArray, (item) => item > 2)).toStrictEqual(false);
+      expect(originalArray).toStrictEqual(copiedArray);
+
+      const callback = jest.fn();
+
+      all.map(originalArray, callback);
+      expect(callback).toHaveBeenNthCalledWith(1, originalArray[0], 0, originalArray);
     })
   })
 });
--- src/js/arrays.js
+++ src/js/arrays.js
@@ -1,9 +1,32 @@
 function forEach(array, callback) {
-  
+
 }
 
 function map(array, callback) {
 
 }
 
-module.exports = map;
+function filter(array, callback) {
+
+}
+
+function reduce(array, callback, initialValue) {
+
+}
+
+function some(array, callback) {
+
+}
+
+function every(array, callback) {
+
+}
+
+module.exports = {
+  forEach,
+  map,
+  filter,
+  reduce,
+  some,
+  every,
+};
